b12901513b65e8f97c59b2e8db9b2793560b9bd4
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-02-24 13:41:27 -0800

    added map for monster state array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [monster, setMonster] = useState([
+        {name: 'Linda',},
+        {name: 'Frank'},
+        {name: 'Jacky'}
+    ],
+);
+
+return (
+    <div className="App container-lg">
+        {monster.map(
+            (monster, index) => <h1 key={index}>{monster.name}</h1>
+        )}
+
+
     </div>
-  );
+);
 }
 
 export default App;
