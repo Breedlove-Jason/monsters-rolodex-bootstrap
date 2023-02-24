89c97541ce7c7c8a200341c7611f3ea5ceae36eb
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-02-24 14:20:39 -0800

    added api call for user data within useEffect state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
+import axios from 'axios';
 
 function App() {
     const [monster, setMonster] = useState([
-        {name: 'Linda',},
-        {name: 'Frank'},
-        {name: 'Jacky'}
+        {name: 'Linda', id: 1},
+        {name: 'Frank', id: 2},
+        {name: 'Jacky', id: 3},
     ],
 );
 
+    const [users, setUsers] = useState([]);
+    // https://jsonplaceholder.typicode.com/users
+
+    useEffect(() => {
+        return () => {
+            const getUsers = () => {
+                axios.get('https://jsonplaceholder.typicode.com/users')
+                    .then((response) => {
+                        setUsers(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+
+            getUsers();
+        };
+    }, []);
+    // const getUsers = () => {
+    //     axios.get('https://jsonplaceholder.typicode.com/users')
+    //         .then((response) => {
+    //             setUsers(response.data);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
+    console.log(users);
 return (
     <div className="App container-lg">
-        {monster.map(
-            (monster, index) => <h1 key={index}>{monster.name}</h1>
+        {users.map(
+            (users, index) => <h1 key={index}>{users.name}</h1>
         )}
 
+        {/*<button onClick={}>Get Users</button>*/}
 
     </div>
 );
