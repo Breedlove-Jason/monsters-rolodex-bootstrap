3ca35d898e81571aaa5231a1aff0776e2e7cfb3f
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-02-25 10:35:50 -0800

    created state for filtered monsters and search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {useState, useEffect} from 'react';
-import {Card, Container} from 'react-bootstrap';
+// import {Card, Container} from 'react-bootstrap';
 import axios from 'axios';
-import { UilSearch } from '@iconscout/react-unicons'
+
 function App() {
     const [monsters, setMonsters] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters);
     // https://jsonplaceholder.typicode.com/users
     useEffect(() => {
         return () => {
@@ -21,27 +23,30 @@ function App() {
             getUsers();
         };
     }, []);
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField]);
+
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLowerCase();
+        setSearchField(searchFieldString);
+    };
+
     return (
         <div className="App container-md">
             <h1>Monsters Rolodex</h1>
 
             <div className="input-group mb-3">
-                <input type={"search"} placeholder={"search monsters"} onChange={(event) => {
-                   const searchString = event.target.value.toLowerCase();
-                    const filteredMonsters = monsters.filter((monster) => {
-                       return  monster.name.toLowerCase().includes(event.target.value)
-                   });
-                    setMonsters(filteredMonsters)
-                }} />
+                <input type={"search"} placeholder={"search monsters"} onChange={onSearchChange} />
                 <button id="button-addon1" type="submit" className="btn btn-primary">Search</button>
             </div>
 
-            {monsters.map(
+            {filteredMonsters.map(
                 (users, id) => <h1 key={users.id}>{users.name}<br/>{users.email}</h1>
             )}
-
-            {/*<button onClick={}>Get Users</button>*/}
-
         </div>
     );
 }
